3ff5f0184b86d5bb2cac99d2d667792010517588
AtomTesli <149719777+AtomTesli@users.noreply.github.com>
2024-03-05 19:45:16 +0500

    Изменение иконки и т.д

--- app/src/main/java/com/example/fastint/Sign_in.java
+++ app/src/main/java/com/example/fastint/Sign_in.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Sign_in extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_in);
+        setContentView(R.layout.activity_sign_in);
     }
     public void startSU (View v) {
         Intent to_sign_up = new Intent(this, Sign_up.class);
