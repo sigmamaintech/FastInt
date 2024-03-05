8e9f66fc68421c3cd33da0bcf3829d85e6bcd185
Lays <FastIntProject@proton.me>
2024-03-05 17:37:29 +0500

    выбор класса + пустые новости

--- app/src/main/java/com/example/fastint/Sign_up2.java
+++ app/src/main/java/com/example/fastint/Sign_up2.java
@@ -3,12 +3,19 @@ package com.example.fastint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Sign_up2 extends AppCompatActivity {
-
+    String[] classes = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up2);
+
+        Spinner selectClass = findViewById(R.id.selectClass);
+        ArrayAdapter<String> selectClassAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, classes);
+        selectClassAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        selectClass.setAdapter(selectClassAdapter);
     }
 }
