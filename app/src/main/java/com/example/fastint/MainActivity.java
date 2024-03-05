7a4307dab54cbe1aa4c3bf8b07c7e8b319e97be9
Lays <FastIntProject@proton.me>
2024-03-05 19:23:46 +0500

    нижнее меню

--- app/src/main/java/com/example/fastint/MainActivity.java
+++ app/src/main/java/com/example/fastint/MainActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
