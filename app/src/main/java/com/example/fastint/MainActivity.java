7ff2841b3102d980e0a6c90e80d258d9e4ba1ad3
Lays <FastIntProject@proton.me>
2024-03-07 19:29:30 +0500

    стилистическая правка

--- app/src/main/java/com/example/fastint/MainActivity.java
+++ app/src/main/java/com/example/fastint/MainActivity.java
@@ -35,8 +35,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void replaceFragment(Fragment fragment)
-    {
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
