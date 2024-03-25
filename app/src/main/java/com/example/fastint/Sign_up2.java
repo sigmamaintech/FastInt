194a8052788b80a93dc3fa60fc1208086f7ee137
AtomTesli <149719777+AtomTesli@users.noreply.github.com>
2024-03-25 19:25:10 +0500

    Опять переписал все, по гайду теперь сделал, но у меня ничего не добавилось(((... Не работает.

--- app/src/main/java/com/example/fastint/Sign_up2.java
+++ app/src/main/java/com/example/fastint/Sign_up2.java
@@ -14,36 +14,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fastint.databinding.ActivitySignUp2Binding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Sign_up2 extends AppCompatActivity {
     private ActivitySignUp2Binding binding;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
     String[] classes = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUp2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
         binding.CMPRegBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (binding.NameSET.getText().toString().isEmpty() || binding.SurnameSET.getText().toString().isEmpty() || binding.selectClass.toString().isEmpty() || (!binding.RBTeacher.isChecked() && !binding.RBStudent.isChecked())) {
                     Toast.makeText(Sign_up2.this,  "Заполните все поля", Toast.LENGTH_SHORT).show();
                 } else {
-                    Boolean isTeach;
+                    boolean isTeach;
                     if (binding.RBStudent.isChecked()) {
                         isTeach = false;
                     } else {
                         isTeach = true;
                     }
-                    User user = new User();
-                    user.writeNewUser(Sign_up.Reglogin, Sign_up.Regemail, Sign_up.Regpassword, binding.NameSET.getText().toString(), binding.SurnameSET.getText().toString(), 1, isTeach);
-                    startActivity(new Intent(Sign_up2.this, MainActivity.class));
-                    Toast.makeText(Sign_up2.this, "Регистрация не удалась", Toast.LENGTH_LONG).show();
+                    auth.createUserWithEmailAndPassword(Sign_up.Regemail, Sign_up.Regpassword)
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    User user = new User();
+                                    user.setUsername(Sign_up.Reglogin);
+                                    user.setName(binding.NameSET.toString());
+                                    user.setSurname(binding.SurnameSET.toString());
+                                    user.setPassword(Sign_up.Regpassword);
+                                    user.setTeacher((Boolean) isTeach);
+                                    user.setSelclass(1);
+                                    users.child(user.getEmail())
+                                            .setValue(user)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Toast.makeText(Sign_up2.this, "Пользователь добавлен!", Toast.LENGTH_SHORT).show();
+                                                    startActivity(new Intent(Sign_up2.this, MainActivity.class));
+                                                }
+                                            });
 
+                                }
+                            });
                 }
             }
         });
--- app/src/main/java/com/example/fastint/User.java
+++ app/src/main/java/com/example/fastint/User.java
@@ -39,19 +39,59 @@ public class User {
         this.isTeacher = isTeacher;
     }
 
-    public void writeNewUser(String username, String email, String password, String name, String surname, Integer selclass, Boolean isTeacher) {
-        //DatabaseReference UserDatabase;
-        //UserDatabase = FirebaseDatabase.getInstance().getReference();
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            User user = new User(username, email, password, name, surname, selclass, isTeacher);
-                            FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(user);
-                        }
-                    }
-                });
+    public Boolean getTeacher() {
+        return isTeacher;
+    }
+
+    public void setTeacher(Boolean teacher) {
+        isTeacher = teacher;
+    }
+
+    public Integer getSelclass() {
+        return selclass;
+    }
+
+    public void setSelclass(Integer selclass) {
+        this.selclass = selclass;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
